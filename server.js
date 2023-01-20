1afde74818be458894efb32f11ec911ad7d95e4f
SRK-prog <srksivaram262@gmail.com>
2023-01-20 23:20:43 +0530

    change

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan(":method :url :response-time"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 mongoose
   .connect(process.env.MONGO_URL)
