893f7aee1e99e4a68bccb18c93f6715096f38bb2
SRK-prog <srksivaram262@gmail.com>
2023-01-24 22:03:19 +0530

    changes & readme updated

--- server.js
+++ server.js
@@ -1,47 +1,34 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const mongoose = require("mongoose");
-const authRoute = require("./routes/auth");
-const userRoute = require("./routes/users");
-const postRoute = require("./routes/posts");
-const conversationRoute = require("./routes/conversations");
-const messageRoute = require("./routes/messages");
-const mailRoute = require("./routes/mails");
-const searchRoute = require("./routes/search");
-const commentRoute = require("./routes/comment");
-const resetPassword = require("./routes/resetPassword");
 const cors = require("cors");
 const morgan = require("morgan");
+const connectDB = require("./connections/connectDB")
+
+dotenv.config();
 
 const app = express();
-dotenv.config();
+
 app.use(express.json());
 app.use(cors());
-app.use(morgan(":method :url :response-time"));
+const isDev = process.env.NODE_ENV === "development";
+const logFormat = isDev
+  ? "dev"
+  : '":method :url HTTP/:http-version" :status ":referrer" ":user-agent" [:date]';
 
-const PORT = process.env.PORT || 8080;
+app.use(morgan(logFormat));
 
-mongoose
-  .connect(process.env.MONGO_URL)
-  .then(() => console.log("connected to mongo"))
-  .catch((err) => console.log(err));
-
-app.get("/", (req, res) => res.send("Microservice Running"));
-
-app.use("/api/auth", authRoute);
-app.use("/api/users", userRoute);
-app.use("/api/posts", postRoute);
-app.use("/api/conversations", conversationRoute);
-app.use("/api/messages", messageRoute);
-app.use("/api/mail", mailRoute);
-app.use("/api/search", searchRoute);
-app.use("/api/comments", commentRoute);
-app.use("/api/reset", resetPassword);
-
-app.listen(PORT, () => {
-  console.log("server running on ", PORT);
-
-  if (process.env.NODE_ENV === "development") {
-    console.log("local: ", `http://localhost:${PORT}/`);
+async function listen() {
+  const PORT = process.env.PORT || 8080;
+  try {
+    require("./routes")(app);
+    await Promise.all([connectDB()]);
+    app.listen(PORT, () => {
+      if (isDev) console.log("local: ", `http://localhost:${PORT}/`);
+    });
+    console.log("Server start on port: ", PORT);
+  } catch (error) {
+    console.log("Failed to start server: ", error);
   }
-});
+}
+
+listen();
